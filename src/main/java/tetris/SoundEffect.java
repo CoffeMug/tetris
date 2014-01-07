287aacdd5856bfe991fc05e82e609bc6b6c5c8ec
coffeMug <aminkhorsandi@gmail.com>
2014-01-07 22:41:03 +0100

    Fixed indentation of the file.

--- src/main/java/tetris/SoundEffect.java
+++ src/main/java/tetris/SoundEffect.java
@@ -16,67 +16,67 @@ import java.net.URISyntaxException;
  * 4. You can use the static variable SoundEffect.volume to mute the sound.
  */
 public enum SoundEffect {
-   BOOM("water.wav"),
-   CHIME("chime.wav");
+	BOOM("water.wav"),
+		CHIME("chime.wav");
 
-   public static enum Volume {
-      MUTE, LOW, MEDIUM, HIGH
-   }
+	public static enum Volume {
+		MUTE, LOW, MEDIUM, HIGH
+			}
 
-   public static Volume volume = Volume.LOW;
+	public static Volume volume = Volume.LOW;
 
-   private Clip clip;
+	private Clip clip;
 
-   SoundEffect(String soundFileName) {
-      try {
-         URL url = this.getClass().getClassLoader().getResource("waves/"+soundFileName);
-         File soundFile = null;
-         InputStream inStream = null;
-		 AudioInputStream soundIn = null;
+	SoundEffect(String soundFileName) {
+		try {
+			URL url = this.getClass().getClassLoader().getResource("waves/"+soundFileName);
+			File soundFile = null;
+			InputStream inStream = null;
+			AudioInputStream soundIn = null;
 
-         if (url.toString().contains("jar")) {
-			 try {
-				 URLConnection uc = url.openConnection();
-				 inStream = uc.getInputStream(); 
-				 InputStream bufferedIn = new BufferedInputStream(inStream);
-				 soundIn = AudioSystem.getAudioInputStream(bufferedIn);
-			 } catch (IOException ex) {
-				 System.err.println(ex);
-			 }
-		 }
-		 else{
-             try {
-			 soundFile = new File(url.toURI());
-             soundIn = AudioSystem.getAudioInputStream(soundFile);
-             } catch (URISyntaxException ex) {
-				 System.err.println(ex);
-			 }
-		 }
+			if (url.toString().contains("jar")) {
+				try {
+					URLConnection uc = url.openConnection();
+					inStream = uc.getInputStream(); 
+					InputStream bufferedIn = new BufferedInputStream(inStream);
+					soundIn = AudioSystem.getAudioInputStream(bufferedIn);
+				} catch (IOException ex) {
+					System.err.println(ex);
+				}
+			}
+			else{
+				try {
+					soundFile = new File(url.toURI());
+					soundIn = AudioSystem.getAudioInputStream(soundFile);
+				} catch (URISyntaxException ex) {
+					System.err.println(ex);
+				}
+			}
 
-         AudioFormat format = soundIn.getFormat();
-         DataLine.Info info = new DataLine.Info(Clip.class, format);
-         clip = (Clip)AudioSystem.getLine(info);
-         clip.open(soundIn);
+			AudioFormat format = soundIn.getFormat();
+			DataLine.Info info = new DataLine.Info(Clip.class, format);
+			clip = (Clip)AudioSystem.getLine(info);
+			clip.open(soundIn);
 
-      } catch (UnsupportedAudioFileException e) {
-         e.printStackTrace();
-      } catch (IOException e) {
-         e.printStackTrace();
-      } catch (LineUnavailableException e) {
-         e.printStackTrace();
-      }
-   }
+		} catch (UnsupportedAudioFileException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (LineUnavailableException e) {
+			e.printStackTrace();
+		}
+	}
 
-   public void play() {
-      if (volume != Volume.MUTE) {
-         if (clip.isRunning())
-            clip.stop();   
-         clip.setFramePosition(0); 
-         clip.start();   
-      }
-   }
+	public void play() {
+		if (volume != Volume.MUTE) {
+			if (clip.isRunning())
+				clip.stop();   
+			clip.setFramePosition(0); 
+			clip.start();   
+		}
+	}
 
-   static void init() {
-      values(); 
-   }
+	static void init() {
+		values(); 
+	}
 }
